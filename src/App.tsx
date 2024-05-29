6fa67f9350e0da7af84d0fa8483526390aa48989
anthonybartczak <to0oni456@gmail.com>
2024-05-30 00:55:47 +0200

    Sync query string state between parent and child components

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,18 @@ import React, { useState } from 'react';
 import parseQueryString from './utils/parseQueryString';
 import ScrollToTopButton from './components/ScrollToTopButton';
 import ParameterDisplay from './components/ParameterDisplay';
-import { Badge } from "./components/ui/badge"
+import { Badge } from './components/ui/badge';
+
+type ParsedQueryString = {
+  name: string;
+  value: string;
+};
 
 const App: React.FC = () => {
-  const [input, setInput] = useState<string>('action_name=Antoni%20Bartczak%20%7C%20Web%20Analytics%20%26%20App%20Dev&idsite=44d572e2-70d8-48dc-85c5-a33c68e36c50&rec=1&r=071186&h=1&m=39&s=5&url=https%3A%2F%2Fwww.anteriam.pl%2F&_id=ac9223a0ee34a41c&_idts=1709667088&_idvc=35&_idn=0&_viewts=1716075539&send_image=0&ts_n=jstc_tm&ts_v=2.20.1&pdf=1&qt=0&realp=0&wma=0&dir=0&fla=0&java=0&gears=0&ag=0&cookie=1&res=2328x1310&dimension2=&gt_ms=35&pv_id=ddo4Ev');
-  const [parsedQueryString, setParsedQueryString] = useState<{ name: string; value: string; }[]>([]);
+  const [input, setInput] = useState<string>(
+    'action_name=Antoni%20Bartczak%20%7C%20Web%20Analytics%20%26%20App%20Dev&idsite=44d572e2-70d8-48dc-85c5-a33c68e36c50&rec=1&r=071186&h=1&m=39&s=5&url=https%3A%2F%2Fwww.anteriam.pl%2F&_id=ac9223a0ee34a41c&_idts=1709667088&_idvc=35&_idn=0&_viewts=1716075539&send_image=0&ts_n=jstc_tm&ts_v=2.20.1&pdf=1&qt=0&realp=0&wma=0&dir=0&fla=0&java=0&gears=0&ag=0&cookie=1&res=2328x1310&dimension2=&gt_ms=35&pv_id=ddo4Ev'
+  );
+  const [parsedQueryString, setParsedQueryString] = useState<ParsedQueryString[]>([]);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInput(event.target.value);
@@ -25,34 +32,39 @@ const App: React.FC = () => {
   };
 
   return (
-    <div className='flex flex-col my-10 mx-10 justify-center gap-y-4'>
+    // Make this div below always keep the same width
+    <div className="flex flex-col my-10 px-8 mx-10 justify-center gap-y-4 min-w-full">
       <ScrollToTopButton />
-      <form onSubmit={handleSubmit} className='flex flex-row gap-x-2'>
+      <form onSubmit={handleSubmit} className="flex flex-row gap-x-2 justify-center min-w-full">
         <input
           type="text"
           value={input}
           onChange={handleInputChange}
           placeholder="Enter Piwik PRO query string"
-          className="p-3 border border-slate-800 rounded"
+          className="p-3 border border-slate-800 rounded min-w-max w-full"
         />
         <button type="submit" className="bg-blue-700 text-white rounded">
           Submit
         </button>
       </form>
-      <div id="badges-container" className='flex flex-row flex-wrap gap-1 justify-stretch'>
+      <div id="badges-container" className="flex flex-row flex-wrap gap-1 justify-stretch">
         {parsedQueryString.map((param) => (
-            <Badge key={param.name} className='text-sm rounded-md bg-slate-900 hover:bg-slate-800'onClick={() => {
-            const element = document.getElementById(param.name);
-            element?.scrollIntoView({
-              behavior: 'smooth'
-            });
-          }}>
-              {param.name}={decodeURIComponent(param.value)}
-            </Badge>
+          <Badge
+            key={param.name}
+            className="cursor-pointer text-sm rounded-md bg-slate-900 hover:bg-slate-800"
+            onClick={() => {
+              const element = document.getElementById(param.name);
+              element?.scrollIntoView({
+                behavior: 'smooth',
+              });
+            }}
+          >
+            {param.name}={decodeURIComponent(param.value)}
+          </Badge>
         ))}
       </div>
-      <div className='flex flex-col gap-y-2'>
-          <ParameterDisplay parsedQueryString={parsedQueryString} />
+      <div className="flex flex-col gap-y-2">
+        <ParameterDisplay parsedQueryString={parsedQueryString} setParsedQueryString={setParsedQueryString} />
       </div>
     </div>
   );
--- src/components/ParameterDisplay.tsx
+++ src/components/ParameterDisplay.tsx
@@ -1,17 +1,6 @@
+import React, { useState, useEffect } from 'react';
 import Markdown from 'react-markdown';
 import docs from '../utils/docs.json';
-import { useState, useEffect } from 'react';
-
-type Parameter = {
-  name: string;
-  in: string;
-  description: string;
-  schema: Schema;
-  example: string | number | string[];
-  deprecated?: boolean;
-  required?: boolean;
-  apiValue: string;
-};
 
 type Schema = {
   type: string;
@@ -25,12 +14,28 @@ type Schema = {
   oneOf?: Schema[];
 };
 
+type Parameter = {
+  name: string;
+  in: string;
+  description: string;
+  schema: Schema;
+  example: string | number | string[];
+  deprecated?: boolean;
+  required?: boolean;
+  apiValue: string;
+};
+
 type ParsedQueryString = {
   name: string;
   value: string;
 };
 
-function ParameterDisplay({ parsedQueryString }: { parsedQueryString: ParsedQueryString[] }) {
+interface ParameterDisplayProps {
+  parsedQueryString: ParsedQueryString[];
+  setParsedQueryString: React.Dispatch<React.SetStateAction<ParsedQueryString[]>>;
+}
+
+const ParameterDisplay: React.FC<ParameterDisplayProps> = ({ parsedQueryString, setParsedQueryString }) => {
   const [foundParameters, setFoundParameters] = useState<Parameter[]>([]);
 
   useEffect(() => {
@@ -54,50 +59,54 @@ function ParameterDisplay({ parsedQueryString }: { parsedQueryString: ParsedQuer
     setFoundParameters(compareParameters());
   }, [parsedQueryString]);
 
-
   const removeParameter = (parameterName: string) => {
-    setFoundParameters(foundParameters.filter((param: Parameter) => param.name !== parameterName));
-  }
+    setFoundParameters(foundParameters.filter((param) => param.name !== parameterName));
+    setParsedQueryString(parsedQueryString.filter((param) => param.name !== parameterName));
+  };
 
   return (
-    foundParameters.length > 0 && (
-      <>
-      {foundParameters.map((parameter: Parameter) => (
-        <div id={parameter.name} className="relative border rounded-md parameter-block gap-y-1.5" key={parameter.name}>
-          <div className='flex flex-row gap-2'>
-            <strong>Parameter:</strong>
-            <code>{parameter.name}</code>
-          </div>
-          <div className='flex flex-row gap-2'>
-            <strong>Example:</strong>
-            <code>{parameter.example}</code>
-          </div>
-          <div className='flex flex-row gap-2'>
-            <strong>Current value:</strong>
-            <code>{parameter.apiValue}</code>
-            <button
-              onClick={() => {
-                const decodedValue = decodeURIComponent(parameter.apiValue);
-                if (decodedValue !== null) {
-                  parameter.apiValue = decodedValue;
-                  setFoundParameters([...foundParameters]);
-                }
-              }}
-              className="text-xs py-0.5 px-1 bg-slate-950 transition duration-300 hover:bg-slate-800">
-              Decode
-            </button>
-          </div>
-          <Markdown>{parameter.description}</Markdown>
-          <button
-            onClick={() => removeParameter(parameter.name)}
-            className="absolute top-0 right-0 m-2 text-xs bg-slate-950 transition duration-300 hover:bg-slate-800">
-            ❌
-          </button>
-        </div>
-      ))}
+    <>
+      {foundParameters.length > 0 && (
+        <>
+          {foundParameters.map((parameter) => (
+            <div id={parameter.name} className="relative min-w-full border rounded-md parameter-block gap-y-1.5" key={parameter.name}>
+              <div className="flex flex-row gap-2">
+                <strong>Parameter:</strong>
+                <code>{parameter.name}</code>
+              </div>
+              <div className="flex flex-row gap-2">
+                <strong>Example:</strong>
+                <code>{parameter.example}</code>
+              </div>
+              <div className="flex flex-row gap-2">
+                <strong>Current value:</strong>
+                <code>{parameter.apiValue}</code>
+                <button
+                  onClick={() => {
+                    const decodedValue = decodeURIComponent(parameter.apiValue);
+                    if (decodedValue !== null) {
+                      parameter.apiValue = decodedValue;
+                      setFoundParameters([...foundParameters]);
+                    }
+                  }}
+                  className="text-xs py-0.5 px-1 bg-slate-950 transition duration-300 hover:bg-slate-800"
+                >
+                  Decode
+                </button>
+              </div>
+              <Markdown>{parameter.description}</Markdown>
+              <button
+                onClick={() => removeParameter(parameter.name)}
+                className="absolute top-0 right-0 m-2 text-xs bg-slate-950 transition duration-300 hover:bg-slate-800"
+              >
+                ❌
+              </button>
+            </div>
+          ))}
+        </>
+      )}
     </>
-    )
   );
-}
+};
 
 export default ParameterDisplay;
