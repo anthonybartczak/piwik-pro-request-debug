a7ff21a93eaa691d3ce4b2a250d4adf4d2cb34ef
anthonybartczak <to0oni456@gmail.com>
2024-06-16 20:28:52 +0200

    Added Piwik PRO, sitemap and robots

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import PiwikPro from "@piwikpro/react-piwik-pro";
+
+PiwikPro.initialize(
+  "01eb8b1c-0f3f-4416-ae37-a9562f6fabb8",
+  "https://abartczak.piwik.pro",
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
