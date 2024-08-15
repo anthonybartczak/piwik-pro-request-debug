c20983065f371b0afbaec8357d1a26eb61178353
anthonybartczak <to0oni456@gmail.com>
2024-08-16 01:39:36 +0200

    Added a guide how to get the request body

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,13 @@ import { Switch } from "./components/ui/switch";
 import { useLocalStorage } from "usehooks-ts";
 
 import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "./components/ui/accordion";
+
+import {
   Copy,
   Github,
   Info,
@@ -82,49 +89,148 @@ const App: React.FC = () => {
     <div className="mt-10 flex min-w-full flex-col justify-center gap-y-2 px-8">
       <ScrollToTopButton />
       {parsedQueryString.length === 0 && (
-        <div className="flex flex-row gap-x-2">
-          <div className="flex w-3/4 flex-col gap-y-2 text-wrap rounded-sm border border-slate-700/[.65] bg-slate-900 p-4 shadow-md">
-            <div className="flex flex-row items-center gap-x-1">
+        <>
+          <div className="flex flex-row gap-x-2">
+            <div className="flex w-3/4 flex-col gap-y-2 text-wrap rounded-sm border border-slate-700/[.65] bg-slate-900 p-4 shadow-md">
+              <div className="flex flex-row items-center gap-x-1.5">
+                <Info className="h-5 w-5" />
+                <h1 className="font-mono text-lg uppercase">What is this?</h1>
+              </div>
+              <Separator decorative className="bg-gray-800" />
+              <p className="font-thin">
+                Request Debugger is a simple web application designed to assist
+                users in debugging and troubleshooting requests made to the
+                Piwik PRO HTTP Tracking API. It provides a user-friendly
+                interface that allows to easily analyze and visualize the
+                details of API requests.
+              </p>
+            </div>
+            <div className="flex w-1/4 flex-col gap-y-2 rounded-sm border border-slate-700/[.65] bg-slate-900 p-4 shadow-md">
+              <div className="flex flex-row items-center gap-x-1.5">
+                <CircleUser className="h-5 w-5" />
+                <h1 className="font-mono text-lg uppercase">Contact me</h1>
+              </div>
+              <Separator decorative className="bg-gray-800" />
+              <ul className="flex flex-col gap-y-2">
+                <li className="flex gap-x-2">
+                  <Github className="h-5 w-5" />
+                  <a href="https://github.com/anthonybartczak/piwik-pro-request-debug">
+                    Github
+                  </a>
+                </li>
+                <li className="flex gap-x-2">
+                  <Linkedin className="h-5 w-5" />
+                  <a href="https://www.linkedin.com/in/antoni-bartczak-525187183/">
+                    LinkedIn
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="flex flex-col gap-y-2 text-wrap rounded-sm border border-slate-700/[.65] bg-slate-900 p-4 shadow-md">
+            <div className="flex flex-row items-center gap-x-1.5">
               <Info className="h-5 w-5" />
-              <span>What is this?</span>
+              <h1 className="font-mono text-lg uppercase">
+                How to get the request details?
+              </h1>
             </div>
             <Separator decorative className="bg-gray-800" />
-            <p className="font-thin">
-              Request Debugger is a simple web application designed to assist
-              users in debugging and troubleshooting requests made to the Piwik
-              PRO HTTP Tracking API. It provides a user-friendly interface that
-              allows to easily analyze and visualize the details of API
-              requests.
-            </p>
+            <Accordion type="single" collapsible className="w-full">
+              <AccordionItem value="item-1" className="">
+                <AccordionTrigger className="bg-slate-900 pb-3 pt-1 text-lg">
+                  Using your browser's developer tools
+                </AccordionTrigger>
+                <AccordionContent className="text-base">
+                  <ol className="rounded-md p-2 font-thin">
+                    <li>1. Go to your website or web application</li>
+                    <li>
+                      2. Press <code>F12</code> to open the developer tools
+                    </li>
+                    <li>
+                      3. Go to the{" "}
+                      <a href="/network.webp" target="_blank" rel="noreferrer">
+                        network tab
+                      </a>
+                    </li>
+                    <li>
+                      4. In the{" "}
+                      <a href="/filter.webp" target="_blank" rel="noreferrer">
+                        filter field
+                      </a>{" "}
+                      type in <code>ppms.php</code>
+                    </li>
+                    <li>
+                      5. If you don't see any results, try refreshing the page
+                      by pressing <code>F5</code>
+                    </li>
+                    <li>
+                      6. Once you see a request, click on it and go to the{" "}
+                      <a href="/payload.webp" target="_blank" rel="noreferrer">
+                        payload tab
+                      </a>
+                    </li>
+                    <li>
+                      7. Click{" "}
+                      <a href="/source.webp" target="_blank" rel="noreferrer">
+                        view source
+                      </a>{" "}
+                      and copy the result.
+                    </li>
+                    <li>
+                      8. Go back to Piwik PRO Request Debugger, paste it into
+                      the input field below and press the submit button
+                    </li>
+                  </ol>
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem value="item-2">
+                <AccordionTrigger className="bg-slate-900 pb-3 pt-1 text-lg">
+                  Using Piwik PRO Tracker Debugger
+                </AccordionTrigger>
+                <AccordionContent className="text-base">
+                  <ol className="rounded-md p-2 font-thin">
+                    <li>
+                      1. Go to your Piwik PRO account and select your website or
+                      application from the list
+                    </li>
+                    <li>
+                      2. Go to the Analytics module, switch to settings and
+                      click on the tracker debugger button
+                    </li>
+                    <li>
+                      3. Click on the{" "}
+                      <a href="/request.webp" target="_blank" rel="noreferrer">
+                        &lt;/&gt;
+                      </a>{" "}
+                      to display event details
+                    </li>
+                    <li>
+                      4. Click{" "}
+                      <a
+                        href="/trackerdebugger.webp"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        copy to clipboard
+                      </a>
+                    </li>
+                    <li>
+                      5. Go back to Piwik PRO Request Debugger, paste it into
+                      the input field below and press the submit button
+                    </li>
+                  </ol>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+            <div className="flex flex-col gap-y-1.5"></div>
           </div>
-          <div className="flex w-1/4 flex-col gap-y-2 rounded-sm border border-slate-700/[.65] bg-slate-900 p-4 shadow-md">
-            <div className="flex flex-row items-center gap-x-1">
-              <CircleUser className="h-5 w-5" />
-              <span>Contact me</span>
-            </div>
-            <Separator decorative className="bg-gray-800" />
-            <ul className="flex flex-col gap-y-2">
-              <li className="flex gap-x-2">
-                <Github className="h-5 w-5" />
-                <a href="https://github.com/anthonybartczak/piwik-pro-request-debug">
-                  Github
-                </a>
-              </li>
-              <li className="flex gap-x-2">
-                <Linkedin className="h-5 w-5" />
-                <a href="https://www.linkedin.com/in/antoni-bartczak-525187183/">
-                  LinkedIn
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        </>
       )}
       <div>
         <div className="flex flex-col gap-y-2 rounded-sm border border-slate-700/[.65] bg-slate-900 p-4 shadow-md">
-          <div className="flex flex-row items-center gap-x-1">
+          <div className="flex flex-row items-center gap-x-1.5">
             <Settings2 className="h-5 w-5" />
-            <span>Settings</span>
+            <h1 className="font-mono text-lg uppercase">Settings</h1>
           </div>
           <Separator decorative className="bg-gray-800" />
           <div className="flex flex-row">
